0cd9d3a3c49d6a3e91302f01b7ad55aaa3598317
BayDev <baydev.contact@gmail.com>
2022-03-14 23:19:37 +0100

    First tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
-const pdf = require('pdf-parse');
-const fs = require('fs');
 const { login } = require('./functions');
 
-login();
+//login();
+
+// first tests for the loop
+setInterval(async () => {
+    console.log(Math.floor(Date.now() / 1000 + 3600) % 86400);
+    if (Math.floor(Date.now() / 1000 + 3600) % 86400 === 83910) console.log('time!');
+}, 1000)
